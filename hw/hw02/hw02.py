ef164571c4940f6327413fc5ab74097f4e6f8690
Jacob <jacobgeorge08@gmail.com>
2024-02-26 10:56:46 +0530

    finished week 4

--- hw/hw02/hw02.py
+++ hw/hw02/hw02.py
@@ -24,6 +24,14 @@ def num_eights(x):
     """
     "*** YOUR CODE HERE ***"
 
+    if x < 10:
+        return 1 if x == 8 else 0
+
+    if(x % 10 == 8):
+        return 1 + num_eights(x // 10)
+    else:
+        return num_eights(x // 10)
+
 
 def pingpong(n):
     """Return the nth element of the ping-pong sequence.
@@ -58,6 +66,33 @@ def pingpong(n):
     True
     """
     "*** YOUR CODE HERE ***"
+    # sum, i = 1,1
+    # boo = True
+    # while i != n:
+    #     if i % 8 == 0 or num_eights(i):
+    #         boo = not boo
+    #     if boo == True:
+    #         sum += 1
+    #     else:
+    #         sum -= 1
+    #     i+=1
+    # return sum
+    def pp_helper(i,bool):
+        if i == n:
+            return 1
+
+        if i % 8 == 0 or num_eights(i):
+            if bool :
+                return -1 + pp_helper(i+1,not bool)
+            else:
+                return 1 + pp_helper(i+1,not bool)
+
+        elif bool:
+            return 1 + pp_helper(i+1,bool)
+        else:
+            return -1 + pp_helper(i+1,bool)
+    return pp_helper(1,True)
+
 
 
 def missing_digits(n):
@@ -88,10 +123,23 @@ def missing_digits(n):
     True
     """
     "*** YOUR CODE HERE ***"
+    def missing_helper(remaining,last_digit):
+
+        if remaining < 10 and last_digit-1 == remaining:
+            return 0
+        if (remaining % 10 == last_digit) or (remaining % 10 == last_digit - 1):
+            return missing_helper(remaining//10,remaining%10)
+        else:
+            return 1 + missing_helper(remaining,last_digit-1)
+    if n <10:
+        return 0
+    else:
+        return missing_helper(n,n%10)
+
 
 
 def next_largest_coin(coin):
-    """Return the next coin. 
+    """Return the next coin.
     >>> next_largest_coin(1)
     5
     >>> next_largest_coin(5)
@@ -120,10 +168,63 @@ def count_coins(total):
     242
     >>> from construct_check import check
     >>> # ban iteration
-    >>> check(HW_SOURCE_FILE, 'count_coins', ['While', 'For'])                                          
+    >>> check(HW_SOURCE_FILE, 'count_coins', ['While', 'For'])
     True
     """
     "*** YOUR CODE HERE ***"
+    def count_helper(total,coin_value):
+        print("Debug:",total,coin_value)
+        if total == coin_value:
+            return 1
+        if total < 0:
+            return 0
+        if coin_value > total:
+            return 0
+
+        ways_using_current_coin = count_helper(total - coin_value,coin_value)
+
+        next_coin = next_largest_coin(coin_value)
+
+        if next_coin:
+            ways_using_next_coin = count_helper(total,next_coin)
+        else:
+            ways_using_next_coin = 0
+
+        return ways_using_current_coin + ways_using_next_coin
+
+    return count_helper(total,1)
+
+
+
+
+
+
+
+
+
+
+
+def any(a, b, pred):
+    """Returns True if any numbers from a to b inclusive satisfy
+    pred.
+
+    >>> any(1, 4, lambda x: x % 2 == 0)
+    True
+    >>> any(-5, 2, lambda x: x * x == -3 * x)   # -3 satisfies pred
+    True
+    >>> any(1, 6, lambda x: x % 7 == 0)
+    False
+    >>> any(0, 6, lambda x: x % 7 == 0)
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
+    if a > b:
+        return False
+    if pred(a):
+        return True
+    return any(a+1,b,pred)
+
 
 
 from operator import sub, mul
@@ -138,5 +239,3 @@ def make_anonymous_factorial():
     >>> check(HW_SOURCE_FILE, 'make_anonymous_factorial', ['Assign', 'AugAssign', 'FunctionDef', 'Recursion'])
     True
     """
-    return 'YOUR_EXPRESSION_HERE'
-

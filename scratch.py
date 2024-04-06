c552a5846abcb7aa61305765e4b0218d13f5f027
T460 <jacobgeorge08@gmail.com>
2024-04-06 10:19:17 +0530

    linked list implementation

---
+++ scratch.py
@@ -0,0 +1,8 @@
+def extend_link(s, t):
+    """Return a list with the elements of s followed by those of t."""
+    assert is_link(s) and is_link(t)
+    if s == empty:
+        return t
+    else:
+        return link(first(s), extend_link(rest(s), t))
+

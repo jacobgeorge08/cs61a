c6a31909214b220e68ad7aaab19a6b130dc0aabb
T460 <jacobgeorge08@gmail.com>
2024-04-06 13:04:05 +0530

    created linked list class

--- scratch.py
+++ scratch.py
@@ -1,8 +1,27 @@
-def extend_link(s, t):
-    """Return a list with the elements of s followed by those of t."""
-    assert is_link(s) and is_link(t)
-    if s == empty:
-        return t
-    else:
-        return link(first(s), extend_link(rest(s), t))
+class Link:
+    """A linked list with a first element and the rest."""
 
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __getitem__(self, i):
+        if i == 0:
+            return self.first
+        else:
+            return self.rest[i - 1]
+
+    def __len__(self):
+        return 1 + len(self.rest)
+
+
+def link_expression(s):
+    """Return a string that would evaluate to s."""
+    if s.rest is Link.empty:
+        rest = ""
+    else:
+        rest = ", " + link_expression(s.rest)
+    return "Link({0}{1})".format(s.first, rest)
